954d80f0b958cf96ccd5165cbfac9216e4f362d9
shasdsa <83131256+shasdsa@users.noreply.github.com>
2021-04-24 19:00:32 +0300

    Update server_data.php

--- assent/server_data.php
+++ assent/server_data.php
@@ -1,7 +1,7 @@
 server|13.67.35.31
 port|17091
 type|1
-#maint|Mainetrance message (Not used for now) -- Growtopia Noobs
+#maint|Mainetrance message (server is mainetrance wait) -- Growtopia Noobs
 
 beta_server|127.0.0.1
 beta_port|17091
